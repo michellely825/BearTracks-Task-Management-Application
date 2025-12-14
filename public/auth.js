31ae1d25bce476aed8398dac923488b911d8df5c
Michelle Ly <michellely825@gmail.com>
2025-12-14 02:11:15 -0800

    refactor: adds sign up / log in buttons to index.html

--- public/auth.js
+++ public/auth.js
@@ -1,3 +1,5 @@
+let charImgIndex = 0;
+
 const params = new URLSearchParams(window.location.search);
 const mode = params.get("mode");
 
@@ -5,6 +7,18 @@ const logInScreen = document.getElementById("log-in");
 const signUpScreen = document.getElementById("sign-up");
 const toLoginButton = document.getElementById("switch-to-login-button");
 const toSignupButton = document.getElementById("switch-to-signup-button");
+const rightArrow = document.getElementById("right-arrow");
+const leftArrow = document.getElementById("left-arrow");
+const charImg = document.getElementById("character-img");
+
+const characters = [
+  "/src/images/characters/bear12.png",
+  "/src/images/characters/bunny12.png",
+  "/src/images/characters/fox12.png",
+  "/src/images/characters/chickenwing12.png",
+  "/src/images/characters/lion12.png",
+  "/src/images/characters/panda12.png",
+];
 
 if (mode === "login") {
   signUpScreen.classList.add("hidden");
@@ -13,12 +27,31 @@ if (mode === "login") {
 }
 
 toLoginButton.addEventListener("click", () => {
-  //   window.location.href = "/auth.html?mode=login";
-  //   logInScreen.classList.remove("hidden");
-  //   signUpScreen.classList.add("hidden");
+  signUpScreen.classList.add("hidden");
+  logInScreen.classList.remove("hidden");
 });
 toSignupButton.addEventListener("click", () => {
-  //   window.location.href = "/auth.html?mode=signup";
-  //   logInScreen.classList.add("hidden");
-  //   signUpScreen.classList.remove("hidden");
+  signUpScreen.classList.remove("hidden");
+  logInScreen.classList.add("hidden");
 });
+
+leftArrow.addEventListener("click", prevCharacter);
+rightArrow.addEventListener("click", nextCharacter);
+
+function nextCharacter() {
+  console.log("next clicked");
+  charImgIndex++;
+  if (charImgIndex >= characters.length) {
+    charImgIndex = 0;
+  }
+  charImg.src = characters[charImgIndex];
+}
+
+function prevCharacter() {
+  console.log("prev clicked");
+  charImgIndex--;
+  if (charImgIndex < 0) {
+    charImgIndex = characters.length - 1;
+  }
+  charImg.src = characters[charImgIndex];
+}
